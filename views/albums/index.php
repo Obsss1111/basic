a070228ca1eb21a3f068a4dbd02da088e4c2ae4f
Danil Smolentsev <coming.dan@yandex.ru>
2021-03-05 19:56:32 +0400

    re design index

--- views/albums/index.php
+++ views/albums/index.php
@@ -10,59 +10,47 @@ use yii\helpers\Url;
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 ?>
-<div class="albums-index">
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $filterModel,
-        'layout' => "{items}\n{pager}",
-        'tableOptions' => ['class' => 'table table-hover table-sm table-borderless container'],
-        'rowOptions' => [
-            'class' => "card text-dark bg-light mb-3",
-//            'style' => "max-width: 540px;",
-        ],
-        'caption' => 'Новые альбомы',
-        'showHeader' => false,
-        'columns' => [
-            [
-                'class' => DataColumn::className(),
-                'attribute' => 'name_album',
-                'format' => 'raw',
-                'options' => ['class' => 'row'],
-                'value' => function($model, $key, $index, $column) {
-                    $btn_options = [
-                        'class' => 'btn btn-light col',
-                        'onclick' => 'albumPlay(this)',
-                    ];
-                    $url_options = [
-                        '/albums/view', 
-                        'id_album' => $model->id_album, 
-                        'autor_id_autor' => $model->autor_id_autor
-                    ];
-                    $html = Html::beginTag('div', ['class' => 'row g-0']).
-                            Html::beginTag('div', ['class' => 'col-md-4']).
-                            Html::button(
-                                    Html::img('http://basic/assets/img/pepegaDance.gif', [
-                                        'class' => 'images-albums'
-                                    ]),
-                                    $btn_options
-                            ).
-                            Html::endTag('div').
-                            Html::beginTag('div', ['class' => 'col-md-8']).
-                            Html::beginTag('div', ['class' => "card-body"]).
-                            Html::beginTag('h5', ['class' => 'card-title']).
-                            Html::a(Html::encode($model->name_album), Url::to($url_options)).
-                            Html::endTag('h5').
-                            "<p class='card-text'>".
-                            Html::a($model->rel_autor->name_autor, Url::to(['/autor/view', 'id' => $model->autor_id_autor])).
-                            "</p>
-                            <p class='card-text'><small class='text-muted'>Last updated 3 mins ago</small></p>".
-                            Html::endTag('div').
-                            Html::endTag('div').
-                            Html::endTag('div').
-                            Html::endTag('div');                            
-                    return $html;
-                },                
-            ],
-        ],
-    ]); ?>
+<div class="row row-cols-1 row-cols-md-2 g-4">
+    <?php foreach ($dataProvider->getModels() as $model) { ?> 
+    <div class="col">        
+        <div class="card mb-3 bg-album" style="border: none;">
+            <button type="button" onclick="albumPlay(this)" class="btn bg-album">            
+            <div class="row g-0">
+                <div class="col-md-6">
+                    <?php if ($model->img) { ?>
+                        <?= Html::img('http://basic/assets/img/'.$model->img, ['class' => 'img-fluid']) ?>
+                    <?php } else { ?>
+                        <svg viewBox="0 0 16 16" class="icon img-fluid img-circle orange-fun border-0 btn">
+                            <use xlink:href="http://basic/assets/iconic/sprite/open-iconic.svg#musical-note"></use>
+                        </svg>
+                    <?php } ?>
+                </div>
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <?= Html::a(
+                                Html::encode($model->name_album), 
+                                ['/albums/view', 'id' => $model->id_album], 
+                                ['class' => 'text-warning text-decoration-none']) 
+                            ?>
+                        </h5>
+                        <p class="card-text">
+                            <?= Html::a(
+                                    $model->autor->name_autor, 
+                                    ['/autor/view', 'id' => $model->autor->id_autor], 
+                                    ['class' => 'text-warning text-decoration-none']) 
+                            ?>
+                        </p>
+                        <p class="card-text">
+                            <small class="text-muted">Количество треков: <?= $model->countTracks ?></small>
+                        </p>
+                    </div>
+                </div>               
+            </div>
+            </button>    
+        </div>
+        
+    </div>
+    <?php } ?>
 </div>
+
