e9a3e3022f1fa82672a812966030cbd2add1e77c
Danil Smolentsev <coming.dan@yandex.ru>
2021-03-05 19:58:20 +0400

    re design Albums.php

--- models/Albums.php
+++ models/Albums.php
@@ -9,8 +9,10 @@ use Yii;
  *
  * @property int $id_album
  * @property string $name_album
- * @property int $autor_id_autor
- * @property int $music_id_music
+ * @property string|null $img
+ *
+ * @property AlbumsMusic[] $albumsMusics
+ * @property FavoriteAlbums[] $favoriteAlbums
  */
 class Albums extends \yii\db\ActiveRecord
 {
@@ -28,10 +30,11 @@ class Albums extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['id_album', 'name_album', 'autor_id_autor', 'music_id_music'], 'required'],
-            [['id_album', 'autor_id_autor', 'music_id_music'], 'integer'],
+            [['id_album', 'name_album'], 'required'],
+            [['id_album'], 'integer'],
             [['name_album'], 'string', 'max' => 45],
-            [['id_album', 'autor_id_autor'], 'unique', 'targetAttribute' => ['id_album', 'autor_id_autor']],
+            [['img'], 'string', 'max' => 64],
+            [['id_album'], 'unique'],
         ];
     }
 
@@ -41,23 +44,40 @@ class Albums extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id_album' => 'Id альбома',
-            'name_album' => 'Альбомы',
-            'autor_id_autor' => 'Id исполнителя',
-            'music_id_music' => 'Id трека',
+            'id_album' => 'Id Album',
+            'name_album' => 'Альбом',
+            'img' => 'Img',
+            'autor.name_autor' => 'Исполнитель'
         ];
+    }
+    
+    public function getAutor()
+    {
+        return $this->hasOne(Autor::class, ['id_autor' => 'id_autor_music'])->viaTable(AlbumsMusic::tableName(), ['id_albums_music' => 'id_album']);
+    }
+    
+    public function getCountTracks() : int
+    {
+        return $this->hasMany(AlbumsMusic::className(), ['id_albums_music' => 'id_album'])->count();
+    }        
+
+    /**
+     * Gets query for [[AlbumsMusics]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAlbumsMusics()
+    {
+        return $this->hasMany(AlbumsMusic::class, ['id_albums_music' => 'id_album']);
     }
 
     /**
-     * {@inheritdoc}
-     * @return AlbumsQuery the active query used by this AR class.
+     * Gets query for [[FavoriteAlbums]].
+     *
+     * @return \yii\db\ActiveQuery
      */
-    public static function find()
+    public function getFavoriteAlbums()
     {
-        return new AlbumsQuery(get_called_class());
-    }
-     
-    public function getRel_autor() {
-        return $this->hasOne(Autor::className(), ['id_autor' => 'autor_id_autor']);
+        return $this->hasMany(FavoriteAlbums::className(), ['albums_id_album' => 'id_album']);
     }
 }
