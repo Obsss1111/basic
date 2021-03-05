fc5c126fe1b5334462a3bd194671f2d6119dab54
Danil Smolentsev <coming.dan@yandex.ru>
2021-03-05 20:03:45 +0400

    change view/autor/index.php

--- views/autor/index.php
+++ views/autor/index.php
@@ -5,22 +5,31 @@ use yii\grid\GridView;
 use yii\grid\DataColumn;
 use yii\helpers\Url;
 
+$color = ['warm', 'bighead', 'orange-fun', 'hazel'];
 ?>
 <div class="autor-index">
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $filterModel,
-        'layout' => "{items}\n{pager}",
-        'tableOptions' => ['class' => 'table table-hover table-sm table-borderless container'],
-        'columns' => [
-            [
-                'class' => DataColumn::className(),
-                'attribute' => 'name_autor',
-                'format' => 'raw',
-                'value' => function($model, $key, $index){
-                    return Html::a(Html::encode($model->name_autor), Url::to(['/autor/view', 'id' => $model->id_autor]));
-                },
-            ],
-        ],
-    ]); ?>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <?php foreach ($dataProvider->getModels() as $model) { ?>
+        <div class="col" style="margin-bottom: 20px;">
+            <div class="card h-100 mb-3 bg-album" style="max-width: 20rem; max-height: 20rem;">
+                <?php if ($model->img) { ?>
+                    <img src="<?= 'http://basic/assets/img/'.$model->img ?>" class="card-img-top">
+                <?php } else { ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-fill <?= $color[random_int(0, 3)] ?>" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    </svg>
+                <?php } ?>
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <?=Html::a(Html::encode($model->name_autor), ['/autor/view', 'id' => $model->id_autor], 
+                            ['class' => 'text-warning text-decoration-none'])?>
+                    </h5>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">Музыкальных треков: <?= $model->countTracks?></small>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+    </div>
 </div>
